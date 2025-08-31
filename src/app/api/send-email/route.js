a0114fc3ff1aa77155397cc788e1855bbef00f59
Saif Ur Rehman <saif.rehman@teo-intl.com>
2025-09-01 02:15:32 +0500

    remove unused code

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,8 +1,4 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import nodemailer from 'nodemailer'
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   try {
